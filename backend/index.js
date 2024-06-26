93afdcf6385b97daec89e3f0bd733c6f5bc981db
Vivek Prajapati <106389092+Vivek-23404@users.noreply.github.com>
2024-06-26 14:06:33 +0530

    Update index.js cors

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser())
 
 const corsOptions = {
   // origin : "http://localhost:5173",
-  origin : "expense-tracker-taupe-nine.vercel.app",
+  origin : "https://expense-tracker-taupe-nine.vercel.app",
   methods: "GET,POST,PUT,DELETE,PATCH",
   credentials : true,
   headers: {
