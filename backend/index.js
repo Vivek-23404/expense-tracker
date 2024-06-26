b60c1909f16826a488c8362506aa6b205474eb55
Vivek Prajapati <106389092+Vivek-23404@users.noreply.github.com>
2024-06-26 13:49:19 +0530

    Update cors for frontend deployement index.js backend

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,8 @@ app.use(cookieParser())
 
 
 const corsOptions = {
-  origin : "http://localhost:5173",
+  // origin : "http://localhost:5173",
+  origin : "expense-tracker-taupe-nine.vercel.app",
   methods: "GET,POST,PUT,DELETE,PATCH",
   credentials : true,
   headers: {
