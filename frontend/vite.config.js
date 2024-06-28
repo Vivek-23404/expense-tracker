42b9c4448a0bda9e4cb15946f438284771b8e265
Vivek Prajapati <vivekprajapati23404@gmail.com>
2024-06-28 12:20:14 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       // "/api" : "http://localhost:3000",
-      "/api" : "https://expense-tracker-toa4.onrender.com",
+      // "/api" : "https://expense-tracker-toa4.onrender.com",
     }
   },
   plugins: [react()],
