37d3865e46e9dfc6031bce49d46064eef4f51e46
Vivek Prajapati <vivekprajapati23404@gmail.com>
2024-07-08 19:04:28 +0530

    cookie verification

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ const app = express()
 
 // DB connection
 connectDB()
+app.use(cookieParser())
 const corsOptions = {
   origin : ["https://expense-tracker-taupe-nine.vercel.app","http://localhost:5173"],
   // origin : "*",
@@ -25,7 +26,6 @@ const corsOptions = {
   // },
   
 }
-app.use(cookieParser())
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
